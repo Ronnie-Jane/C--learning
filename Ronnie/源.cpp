e7bb7387e8bece4e6c0eae524e3db7a1edd8cf17
Ronnie-jane <374698465@qq.com>
2020-03-12 16:00:04 +0800

    combine arrays into one

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,6 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>	
+#include<cstring>
+using namespace std;
 int main()
 {
-	std::cout << "hello world!";
+	char f_name[10], l_name[20];
+	cout << "Enter your first name: ";
+	cin >> f_name;
+	cout << "Then enter your last name: ";
+	cin >> l_name;
+	strcat(l_name, " ,");
+	strcat(l_name, f_name);
+	cout << "Here's the information in a single string: " << l_name;
 	return 0;
 }
