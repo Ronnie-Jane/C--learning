37e5cf74172a83d6b7ca34c0e4d5901c4e5c03a6
Ronnie-jane <374698465@qq.com>
2020-03-12 17:07:29 +0800

    "array"

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,24 +1,13 @@
 #include<iostream>	
-#include<cstring>
-#include<string>
+#include<array>
 using namespace std; 
-struct CandyBar
-{
-	char brand[15];
-	float weight;
-	int calorie;
-};
 int main()
 {
-	CandyBar* pc = new CandyBar[3];
-	cout << "Enter a candy brand: ";
-	cin.getline(pc[0].brand, 15);
-	cout << "Enter the weight: ";
-	cin >> pc[0].weight;
-	cout << "Enter the calorie: ";
-	cin >> pc[0].calorie;
-	cout << "Now this is the information of a candy:\n"
-		<< "Brand: " << pc[0].brand << endl << "Weight: " << pc[0].weight << endl
-		<< "Calorie: " << pc[0].calorie;
+	array<float, 3> scores ;
+	cout<<"Enter three results of 50 merters run: ";
+	cin >> scores[0] >> scores[1] >> scores[2];
+	cout << "Here are the results: "
+		<< scores[0] << "\'s\n" << scores[1] << "\'s\n"
+		<< scores[2] << "\'s";
 	return 0;
 }
