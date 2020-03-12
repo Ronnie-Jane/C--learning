42c9a1dc3f5826ac60e6b18d403539b4d3de5921
Ronnie-jane <374698465@qq.com>
2020-03-12 16:29:41 +0800

    use "new" to allocate memory for the pizza structure

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,22 +1,24 @@
 #include<iostream>	
 #include<cstring>
 #include<string>
-using namespace std;
+using namespace std; 
+struct pizzastore
+{
+	char brand[15];
+	int diameter;
+	int weight;
+};
 int main()
 {
-	struct pizzastore
-	{
-		char brand[15];
-		int diameter;
-		int weight;
-	}pizza[10];
+	pizzastore* ps = new pizzastore;
 	cout << "Enter the name of a pizza: ";
-	cin.getline(pizza[0].brand, 15);
+	cin.getline(ps->brand, 15);
 	cout << "Enter the diameter of the pizza: ";
-	cin >> pizza[0].diameter;
+	cin >> ps->diameter;
 	cout << "Enter the weight of the pizza: ";
-	cin >> pizza[0].weight;
+	cin >> (*ps).weight;
 	cout << "This is the information of the pizza you choose: "
-		<< pizza[0].brand << " " << pizza[0].diameter << " " << pizza[0].weight;
+		<< ps->brand << " " << ps->diameter << " " << ps->weight;
+	delete ps;
 	return 0;
 }
