db637ddcf8d6c467417019fb6e808646665959bc
Ronnie-jane <374698465@qq.com>
2020-03-13 11:25:41 +0800

    range-based FOR loop

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,13 +1,19 @@
 #include<iostream>	
-#include<array>
+#include<string>
 using namespace std; 
 int main()
 {
-	array<float, 3> scores ;
-	cout<<"Enter three results of 50 merters run: ";
-	cin >> scores[0] >> scores[1] >> scores[2];
-	cout << "Here are the results: "
-		<< scores[0] << "\'s\n" << scores[1] << "\'s\n"
-		<< scores[2] << "\'s";
+	int num[5] = { 1,2,3,4,5 };
+	for (int x : num)
+		cout << x << " ";
+	cout << endl;
+	for (int& x : num)
+	{
+		x *= x;
+		cout << x << " ";
+	}
+	cout << endl;
+	for (int numm : {1, 3, 5, 7, 9})
+		cout << numm << " ";
 	return 0;
 }
