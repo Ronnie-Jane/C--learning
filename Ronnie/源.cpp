554875b3f073f323e7a43552f61eaae383b1386f
Ronnie-jane <374698465@qq.com>
2020-03-13 21:45:31 +0800

    use FOR to input to a two-dimensional array

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -3,19 +3,23 @@
 using namespace std; 
 int main()
 {
-	string month[12] =				//or char*month[12]=
+	const int Month = 12, Year = 3;
+	int sale[Year][Month], sum[3] = { 0 };
+	cout << "Enter the sales data of each year£º\n";
+	for (int i = 0; i < 3; i++)
 	{
-		"January","February","March","April","May","June","July",
-		"August","September","October","November","December"
-	};
-	int sale[12], sum=0;
-	cout << "Enter the sales of 12 months: ";
-	for (int i = 0; i <= 11; i++)
-	{
-		cout << month[i] << " :___\b\b\b";
-		cin >> sale[i];
-		sum += sale[i];
+		cout << "Year " << i+1 << " : " << endl;
+		for (int j = 0; j <= 11; j++)
+		{
+			cout << j+1 << " : ";
+			cin >> sale[i][j];
+			sum[i] += sale[i][j];
+		}
 	}
-	cout << "During the whole year, " << sum << " books have been sold.";
+	cout << "The total sales of each year are:\n";
+	for (int i = 0; i <= 2; i++)
+	{
+		cout << "Year " << i+1 << " : " << sum[i] << endl;
+	}
 	return 0;
 }
