31bb3f2ddd6727e6d24f383f16e02e1092a17129
Ronnie-jane <374698465@qq.com>
2020-03-14 14:45:43 +0800

    use IF statement to distinguish triangle

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,6 +1,22 @@
-#include<iostream>	
+#include<iostream>
+using namespace std;
 int main()
 {
-	std::cout << "hello world!";
+	int a, b, c;
+	bool d;
+	cout << "Enter three numbers and I will tell if they can compose a triangle:";
+	cin >> a >> b >> c;
+	if (a < b + c&&b<a+c&&c<a+b)
+		d = true;
+	else d = false;
+	if (d == true)
+	{
+		if (a == b && b == c)
+			cout << endl << "This is a equilateral triangle.";
+		else if (a == b || b == c || a == c)
+			cout << endl << "This is a isosceles triangle.";
+		else cout << endl << "This is an ordinary triangle.";
+	}
+	else cout << "These cannot compose a triangle.";
 	return 0;
 }
