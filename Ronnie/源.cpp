6dd17cde4046ca8d3872570e798cb8daccadd99b
Ronnie-jane <374698465@qq.com>
2020-03-18 21:00:49 +0800

    file instream

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,24 +1,20 @@
 #include<iostream>
-#include<cctype>
-using namespace std; 
+#include<fstream>
+using namespace std;
 int main()
 {
-	cout << "Please enter one of the following choices: " << endl
-		<< "A)	B)	" << endl << "C)	D)" << endl;
-	char choice;
-	while (!(cin >> choice&&choice>='A'&&choice<='D'))
+	ifstream fin;
+	fin.open("Galway Girl.txt");
+	char ch;
+	int count = 0;
+	fin.get(ch);
+	while (fin.good())
 	{
-		cin.clear();
-		while (cin.get() != '\n')
-			continue;
-		cout << "Please enter a choice! Now again: ";
+		cout << ch;
+		if (isalpha(ch))
+			count++;
+		fin.get(ch);
 	}
-	switch (choice)
-	{
-	case'A':cout << "Give you an apple."; break;
-	case 'B':cout << "I am a boy."; break;
-	case 'C':cout << "I love Ciri."; break;
-	case 'D':cout << "Naughty Dog"; break;
-	}
+	cout << "File end reached.";
 	return 0;
 }
