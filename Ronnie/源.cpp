cd6e1f0b37c0f303e202151cb8afb57daef829fb
Ronnie-jane <374698465@qq.com>
2020-03-22 11:01:57 +0800

    function of array

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,20 +1,52 @@
 #include<iostream>
-#include<fstream>
 using namespace std;
+const int Max = 10;
+double average(int*,int count);
+int input(int*,int count);
+void output(int*, int count);
 int main()
 {
-	ifstream fin;
-	fin.open("Galway Girl.txt");
-	char ch;
-	int count = 0;
-	fin.get(ch);
-	while (fin.good())
+	int size;
+	double aver;
+	int grade[Max];
+	cout << "Enter no more than 10 integers of your grades and I will "
+		<< "print them followed by the average." << endl;
+	size = input(grade, Max);
+	output(grade, size);
+	aver = average(grade, size)/size;
+	cout << "The average of your grade is: " << aver;
+}
+int input(int* arr, int limit)
+{
+	int i;
+	for ( i = 0; i < limit; i++)
 	{
-		cout << ch;
-		if (isalpha(ch))
-			count++;
-		fin.get(ch);
+		cout << "grade "<<i+1<<" : ";
+		cin >> arr[i];
+		if (!cin)
+		{
+			cout << "Bad in put, process terminated.\n";
+			break;
+		}
 	}
-	cout << "File end reached.";
-	return 0;
+	return i;
+}
+
+void output(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "grade " << i + 1 << " : " << arr[i] << endl;
+	}
+	return;
+}
+
+double average(int arr[], int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
 }
