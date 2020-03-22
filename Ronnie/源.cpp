45b7714b81c4ab64ef124c31ed398eaa911cab7b
Ronnie-jane <374698465@qq.com>
2020-03-22 16:06:59 +0800

    function to cin,cout,and modify array

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,24 +1,52 @@
 #include<iostream>
 using namespace std;
-double factorial(int n);
-void main()
+int fill_array(int[],int Max);
+void show_array(const int*, int size);
+void reserve_array(int[],int size);
+const int Max = 15;
+int main()
 {
-	int n;
-	double fac;
-	cout << "Enter an upright integer: ";
-	cin >> n;
-	fac = factorial(n);
-	cout << "The factorial of integer " << n << " is " << fac;
+	int value[Max];
+	int size = fill_array(value, Max);
+	show_array(value, size);
+	reserve_array(value, size);
+	show_array(value, size);
+	return 0;
 }
 
-double factorial(int n)
+int fill_array(int value[], int Max)
 {
-	double fac=1;
-	if (n > 0)
-		fac = n * factorial(n - 1);
-	else if (n == 0)
-		fac = 1;
-	else if (n < 0)
-		cout << "Please enter an integer!";
-	return fac;
+	int i;
+	cout << "Now enter each values to fill the array:"
+		<<"(enter non-numeric to terminate input)" << endl;
+	for (i = 0; i < Max; i++)
+	{
+		cout << "value #" << i + 1 << ": ";
+		cin >> value[i];
+		if (!cin)
+			break;
+	}
+	return i;
+}
+
+void show_array(const int value[], int size)
+{
+	cout << "The following are your enterings:" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		if (!((i + 1) % 4))
+			cout << endl;
+		cout << "value #" << i + 1 << ": " << value[i]<<"\t";
+	}
+	cout << endl;
+}
+
+void reserve_array(int value[], int size)
+{
+	for (int i = 1; i <= size / 2; i++)
+	{
+		int temp = value[size - i - 1];
+		value[size - i - 1] = value[i];
+		value[i] = temp;
+	}
 }
