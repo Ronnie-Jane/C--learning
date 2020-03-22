89a4cb3cb770c52447c111011a2dbc5c59429d77
Ronnie-jane <374698465@qq.com>
2020-03-22 14:50:50 +0800

    function using recursion

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,47 +1,24 @@
 #include<iostream>
 using namespace std;
-struct box
+double factorial(int n);
+void main()
 {
-	char maker[40];
-	float height;
-	float width;
-	float length;
-	float volume;
-};
-void show(box);
-void calculate(box*);
-void input(box*);
-int main()
-{
-	box aa;
-	input(&aa);
-	show(aa);
-	calculate(&aa);
-	cout << "The volume of your box is: " << aa.volume;
+	int n;
+	double fac;
+	cout << "Enter an upright integer: ";
+	cin >> n;
+	fac = factorial(n);
+	cout << "The factorial of integer " << n << " is " << fac;
 }
 
-void input(box*a)
+double factorial(int n)
 {
-	cout << "What's the maker?";
-	cin >> a->maker;
-	cout << "Enter each value(meter):" << endl
-		<< "height: ";
-	cin >> a->height;
-	cout << "width: ";
-	cin >> a->width;
-	cout << "length: ";
-	cin >> a->length;
-}
-
-void show(const box a)
-{
-	cout << "Your box:" << endl
-		<< "height: " << a.height << " meters" << endl
-		<< "width: " << a.width << " meters" << endl
-		<< "length: " << a.length << " meters" << endl;
-}
-
-void calculate(box* ps)
-{
-	ps->volume = ps->height * ps->width * ps->length;
+	double fac=1;
+	if (n > 0)
+		fac = n * factorial(n - 1);
+	else if (n == 0)
+		fac = 1;
+	else if (n < 0)
+		cout << "Please enter an integer!";
+	return fac;
 }
