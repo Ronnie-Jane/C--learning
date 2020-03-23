c01a743c910079121cf38cc7453688e91acb633f
Ronnie-jane <374698465@qq.com>
2020-03-23 16:11:22 +0800

    functions using array RANGE and CONST

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,52 +1,67 @@
 #include<iostream>
 using namespace std;
-int fill_array(int[],int Max);
-void show_array(const int*, int size);
-void reserve_array(int[],int size);
-const int Max = 15;
+double* fill_array(double*, int);
+void show_array(const double* begin, const double* end);
+void revalue_array(double*, double*,double);
+const int Max = 5;
+
 int main()
 {
-	int value[Max];
-	int size = fill_array(value, Max);
-	show_array(value, size);
-	reserve_array(value, size);
-	show_array(value, size);
+	double properties[Max];
+	double* end = fill_array(properties, Max);
+	show_array(properties, end);
+	if (properties < end)
+	{
+		cout << "Enter revaluation factor: ";
+		double factor;
+		while (!(cin >> factor))
+		{
+			cin.clear();
+			while (cin.get()!= '\n')
+				continue;
+			cout << "Bad input ; Please enter a number: ";
+		}
+		revalue_array(properties, end, factor);
+		show_array(properties, end);
+	}
+	cout << "Done\n";
 	return 0;
 }
 
-int fill_array(int value[], int Max)
+double* fill_array(double* arr, int m)
 {
+	double temp;
 	int i;
-	cout << "Now enter each values to fill the array:"
-		<<"(enter non-numeric to terminate input)" << endl;
 	for (i = 0; i < Max; i++)
 	{
-		cout << "value #" << i + 1 << ": ";
-		cin >> value[i];
+		cout<<"Enter value #"<<i+1<<": ";
+		cin >> temp;
 		if (!cin)
+		{
+			cin.clear();
+			while (cin.get() != '\n')
+				continue;
+			cout << "Bad input;input terminated.\n";
 			break;
+		}
+		else if (temp < 0)
+			break;
+		arr[i] = temp;
 	}
-	return i;
+	return arr+i;
 }
 
-void show_array(const int value[], int size)
+void show_array(const double* begin, const double* end)
 {
-	cout << "The following are your enterings:" << endl;
-	for (int i = 0; i < size; i++)
+	for (int i = 0; begin + i <= end; i++)
 	{
-		if (!((i + 1) % 4))
-			cout << endl;
-		cout << "value #" << i + 1 << ": " << value[i]<<"\t";
+		cout << "Properties #" << i + 1 << ": $"
+			<< begin[i] << endl;
 	}
-	cout << endl;
 }
 
-void reserve_array(int value[], int size)
+void revalue_array(double* arr, double* end,double n)
 {
-	for (int i = 1; i <= size / 2; i++)
-	{
-		int temp = value[size - i - 1];
-		value[size - i - 1] = value[i];
-		value[i] = temp;
-	}
+	for (int i = 0; arr + i <= end; i++)
+		arr[i] *= n;
 }
