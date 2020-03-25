5b07fa3c132531947c6054854408ead8df3d4ccc
Ronnie-jane <374698465@qq.com>
2020-03-25 22:21:31 +0800

    递归

--- Ronnie/源.cpp
+++ Ronnie/源.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
 using namespace std;
+void print(int n);
 int main()
 {
-	cout << "Later I will display all the narcissistic number between 100 and 999: " << endl;
-	int total = 0;
-	for (int num = 100; num < 1000; num++)
-	{
-		int a, b, c;
-		a = num / 100;
-		b = num / 10 % 10;
-		c = num % 10;
-		if (num == c * c * c + b * b * b + a * a * a)
-		{
-			cout << num << endl;
-			total++;
-		}
-	}
-	cout << endl << "A total of " << total << " numbers.";
-	return 0;
+	cout << "Enter an integer: ";
+	int n;
+	cin >> n;
+	print(n);
+}
+
+void print(int n)
+{
+	if (n > 1)
+		print(n-1);
+	for (int i = 0; i < n; i++)
+		cout << n << '\t';
+	cout << endl;
 }
